5a6928819da27d2b18645adcfd6ebe8bf7ad20be
Steven Craig <stevencraig321@gmail.com>
2021-03-18 09:04:19 +0000

    first commit

---
+++ models/author.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Formats Date of Birth
+AuthorSchema
+.virtual('dob_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+});
+
+// Formats Date of Death
+AuthorSchema
+.virtual('dod_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
